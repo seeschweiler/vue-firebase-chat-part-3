230743089a96c588aa0a7edff198359f781d130c
Sebastian Eschweiler <sebastian.eschweiler@gmail.com>
2018-11-11 15:26:46 +0100

    first commit

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import firestore from 'firebase/firestore';
 
 var config = {
-    apiKey: "AIzaSyBBHnR9gQG8Te7UZ0LPD05eaN2peLRydSo",
-    authDomain: "vuejs-firebase-01.firebaseapp.com",
-    databaseURL: "https://vuejs-firebase-01.firebaseio.com",
-    projectId: "vuejs-firebase-01",
-    storageBucket: "vuejs-firebase-01.appspot.com",
-    messagingSenderId: "726812619425"
+    apiKey: "...",
+    authDomain: "...",
+    databaseURL: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "..."
   };
   const firebaseApp = firebase.initializeApp(config);
   firebaseApp.firestore().settings({ timestampsInSnapshots: true});
